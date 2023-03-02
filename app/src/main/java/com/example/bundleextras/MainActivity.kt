664ccea1f3211fad6c7a85546edf3cf261d73c00
yohana-teixeira01 <yteixeira133@gmail.com>
2023-03-02 15:30:13 -0300

    Subindo Arquivos

---
+++ app/src/main/java/com/example/bundleextras/MainActivity.kt
@@ -0,0 +1,24 @@
+package com.example.bundleextras
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.bundleextras.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.buttonOk.setOnClickListener {
+            val numero = binding.editNumero.text.toString().trim().toInt()
+
+            val i = Intent(this,MainActivity2::class.java)
+            i.putExtra("numero1", numero)
+            startActivity(i)
+        }
+    }
+}
